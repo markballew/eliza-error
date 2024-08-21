3924daf4b669eb6e416a0551442b9db96edd160d
moon <shawmakesmagic@gmail.com>
2024-08-20 23:19:40 -0700

    add log state and image rec

--- src/clients/discord/index.ts
+++ src/clients/discord/index.ts
@@ -19,6 +19,7 @@ import {
 } from "bgent";
 import { UUID } from "crypto";
 import {
+  Attachment,
   BaseGuildVoiceChannel,
   ChannelType,
   Client,
@@ -41,10 +42,10 @@ import settings from "../../core/settings.ts";
 import { AudioMonitor } from "./audioMonitor.ts";
 import { commands } from "./commands.ts";
 import { InterestChannels, ResponseType } from "./types.ts";
-import ImageRecognitionService from "../../services/imageRecognition.ts"
-import { extractAnswer } from "../../core/util.ts"; 
 import { SpeechSynthesizer } from "../../services/speechSynthesis.ts";
 import WavEncoder from "wav-encoder";
+import { log_to_file } from "../../core/logger.ts";
+import ImageRecognitionService from "../../services/imageRecognition.ts";
 
 export const messageHandlerTemplate =
 // `{{actionExamples}}
@@ -67,7 +68,7 @@ export const messageHandlerTemplate =
 # INSTRUCTIONS: Write the next message for {{agentName}}.
 \nResponse format should be formatted in a JSON block like this:
 \`\`\`json
-{ \"user\": \"{{agentName}}\", \"responseMessage\": string, \"action\": string }
+{ \"user\": \"{{agentName}}\", \"content\": string, \"action\": string }
 \`\`\``;
 
 export const shouldRespondTemplate =
@@ -101,15 +102,15 @@ export class DiscordClient extends EventEmitter {
   private streams: Map<string, Readable> = new Map();
   private connections: Map<string, VoiceConnection> = new Map();
   private agent: Agent;
-  private bio: string;
+  private character: any;
   private transcriber: any;
-  private imageRecognitionService: ImageRecognitionService;
   speechSynthesizer: SpeechSynthesizer;
+  imageRecognitionService: ImageRecognitionService;
 
-  constructor(agent: Agent, bio: string) {
+  constructor(agent: Agent, character: any) {
     super();
     this.apiToken = settings.DISCORD_API_TOKEN;
-    this.bio = bio;
+    this.character = character;
     this.client = new Client({
       intents: [
         GatewayIntentBits.Guilds,
@@ -127,9 +128,6 @@ export class DiscordClient extends EventEmitter {
 
     this.initializeTranscriber();
 
-    this.imageRecognitionService = new ImageRecognitionService();
-    this.imageRecognitionService.initialize();
-
     this.client.once(Events.ClientReady, async (readyClient: { user: { tag: any; id: any } }) => {
       console.log(`Logged in as ${readyClient.user?.tag}`);
       console.log("Use this URL to add the bot to your server:");
@@ -146,6 +144,11 @@ export class DiscordClient extends EventEmitter {
 
   private async initializeTranscriber() {
     this.transcriber = await transformersPipeline('automatic-speech-recognition', 'Xenova/whisper-tiny.en');
+  }
+
+  private async initializeImageRecognitionService() {
+    this.imageRecognitionService = new ImageRecognitionService(this.agent);
+    await this.imageRecognitionService.initialize();
   }
 
 
@@ -192,6 +195,7 @@ export class DiscordClient extends EventEmitter {
     channel: BaseGuildVoiceChannel,
     audioStream: Readable
   ) {
+    console.log("handleVoiceStateUpdate", user_id, userName, channel, audioStream);
     const channelId = channel.id;
     const userIdUUID = uuid(user_id) as UUID;
     this.listenToSpokenAudio(
@@ -200,9 +204,6 @@ export class DiscordClient extends EventEmitter {
       channelId,
       audioStream,
       async (responseAudioStream) => {
-        responseAudioStream.on("close", () => {
-          console.log("Response audio stream closed");
-        });
         await this.playAudioStream(user_id, responseAudioStream);
       },
       ResponseType.RESPONSE_AUDIO
@@ -216,13 +217,12 @@ export class DiscordClient extends EventEmitter {
     const user_id = message.author.id as UUID;
     const userName = message.author.username;
     const channelId = message.channel.id;
+    const textContent = message.content;
 
     // Check for image attachments
-    if (message.attachments.size > 0) {
-      await this.handleImageRecognition(message);
-    }
-
-    const textContent = message.content;
+    const imageAttachments = message.attachments.find(attachment => 
+      attachment.contentType?.startsWith('image/')
+    );
 
     try {
       const responseStream = await this.respondToText({
@@ -231,6 +231,7 @@ export class DiscordClient extends EventEmitter {
         channelId,
         input: textContent,
         requestedResponseType: ResponseType.RESPONSE_TEXT,
+        imageAttachments: imageAttachments ? [imageAttachments] : [],
         message,
         discordMessage: message,
         discordClient: this.client,
@@ -255,12 +256,12 @@ export class DiscordClient extends EventEmitter {
     if (!interaction.isCommand()) return;
 
     switch (interaction.commandName) {
-      case "setname":
-        await this.handleSetNameCommand(interaction);
-        break;
-      case "setbio":
-        await this.handleSetBioCommand(interaction);
-        break;
+      // case "setname":
+      //   await this.handleSetNameCommand(interaction);
+      //   break;
+      // case "setbio":
+      //   await this.handleSetBioCommand(interaction);
+      //   break;
       case "joinchannel":
         await this.handleJoinChannelCommand(interaction);
         break;
@@ -287,6 +288,13 @@ export class DiscordClient extends EventEmitter {
     };
     const wavArrayBuffer = encode.sync(audioData);
     return wavArrayBuffer;
+  }
+
+  async recognizeImage(imageUrl: string) {
+    if (!this.imageRecognitionService) {
+      await this.initializeImageRecognitionService();
+    }
+    return this.imageRecognitionService.recognizeImage(imageUrl);
   }
 
   async speechToText(audioBuffer: Buffer) {
@@ -328,20 +336,6 @@ export class DiscordClient extends EventEmitter {
     }
   }
 
-  private async handleImageRecognition(message: DiscordMessage) {
-    const attachment = message.attachments.first();
-    if (attachment && attachment.contentType?.startsWith('image/')) {
-      try {
-        const recognizedText = await this.imageRecognitionService.recognizeImage(attachment.url);
-        const description = extractAnswer(recognizedText[0]);
-        // Add the image description to the completion context
-        message.content += `\nImage description: ${description}`;
-      } catch (error) {
-        console.error('Error recognizing image:', error);
-        await message.reply('Sorry, I encountered an error while processing the image.');
-      }
-    }
-  }
   
   private async ensureUserExists(agentId: UUID, userName: string, botToken: string | null = null) {
     if (!userName && botToken) {
@@ -384,6 +378,7 @@ export class DiscordClient extends EventEmitter {
     interestChannels,
     discordClient,
     discordMessage,
+    imageAttachments,
   }: {
     message: Message;
     hasInterest?: boolean;
@@ -394,15 +389,35 @@ export class DiscordClient extends EventEmitter {
     interestChannels?: InterestChannels;
     discordClient: Client;
     discordMessage: DiscordMessage;
+    imageAttachments?: Attachment[];
   }) {
     if (!message.content.content) {
       return { content: "", action: "IGNORE" };
     }
+
+    let imageDescriptions = [];
+
+    if (imageAttachments) {
+      console.log("imageAttachments", imageAttachments);
+      for (const attachment of imageAttachments) {
+        const recognitionResult = await this.recognizeImage(attachment.url);
+        imageDescriptions.push(recognitionResult);
+      }
+    }
+
+    if (imageDescriptions.length > 0) {
+      let counter = 1;
+      for (const description of imageDescriptions) {
+        message.content.content += ` (Attachment ${counter}: ${description})`;
+        counter++;
+      }
+    }
+
     if (!state) {
       state = await this.agent.runtime.composeState(message, {
         discordClient,
         discordMessage,
-        agentName: this.client.user?.displayName,
+        agentName: this.character?.name || this.client.user?.displayName,
       });
     }
 
@@ -416,7 +431,7 @@ export class DiscordClient extends EventEmitter {
     state = await this.agent.runtime.composeState(message, {
       discordClient,
       discordMessage,
-      agentName: this.client.user?.displayName,
+      agentName: this.character?.name || this.client.user?.displayName,
     });
 
     if (!shouldRespond && hasInterest) {
@@ -486,17 +501,29 @@ export class DiscordClient extends EventEmitter {
       state,
       template: shouldRespondTemplate,
     });
-    console.log("Should respond context: ", shouldRespondContext)
-    const response = await this.agent.runtime.completion({
-      context: shouldRespondContext,
-      temperature: 0.1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
 
+    const datestr = new Date().toISOString().replace(/:/g, '-');
+        
+    // log context to file
+    log_to_file(`${state.agentName}_${datestr}_shouldrespond_context`, shouldRespondContext)
+
+    let response;
     
-
-    console.log("*** response from ", state.agentName, ":", response);
+    for (let triesLeft = 3; triesLeft > 0; triesLeft--) {
+      try {
+        response = await this.agent.runtime.completion({
+          context: shouldRespondContext,
+          temperature: 0.1,
+          frequency_penalty: 0.0,
+          presence_penalty: 0.0,
+        });
+        log_to_file(`${state.agentName}_${datestr}_shouldrespond_response`, response)
+      } catch (error) {
+        console.error("Error in _checkShouldRespond:", error);
+        console.error("Error details:", error.message);
+        console.log("Retrying...")
+      }
+    }
 
     if (response.toLowerCase().includes("respond")) {
       return true;
@@ -515,14 +542,30 @@ export class DiscordClient extends EventEmitter {
     let responseContent: Content | null = null;
     const { user_id, room_id } = message;
 
+
+    const datestr = new Date().toISOString().replace(/:/g, '-');
+        
+    // log context to file
+    log_to_file(`${state.agentName}_${datestr}_generate_context`, context)
+
+    let response;
+
     for (let triesLeft = 3; triesLeft > 0; triesLeft--) {
-      console.log("Generating response")
-      const response = await this.agent.runtime.completion({
-        context,
-        stop: [],
-      });
+
+      try {
+        response = await this.agent.runtime.completion({
+          context,
+          stop: [],
+        });
+      } catch (error) {
+        console.error("Error in _generateResponse:", error);
+        console.error("Error details:", error.message);
+        console.log("Retrying...")
+      }
 
       console.log("response is", response)
+
+      log_to_file(`${state.agentName}_${datestr}_generate_response`, response)
 
       const values = {
         body: response,
@@ -681,6 +724,7 @@ export class DiscordClient extends EventEmitter {
     message,
     discordMessage,
     discordClient,
+    imageAttachments,
     interestChannels,
   }: {
     user_id: UUID;
@@ -690,6 +734,7 @@ export class DiscordClient extends EventEmitter {
     requestedResponseType?: ResponseType;
     message?: DiscordMessage;
     discordClient: Client;
+    imageAttachments?: Attachment[];
     discordMessage?: DiscordMessage;
     interestChannels?: InterestChannels;
   }): Promise<Readable | null> {
@@ -728,6 +773,7 @@ export class DiscordClient extends EventEmitter {
       interestChannels,
       discordClient: this.client,
       discordMessage: discordMessage as DiscordMessage,
+      imageAttachments,
     });
 
     const content = (response.responseMessage || response.response || response.content || response.message) as string;
@@ -751,11 +797,11 @@ export class DiscordClient extends EventEmitter {
     }
 
     // set the bio back to default
-    if (this.bio) {
+    if (this.character?.bio) {
       adapter.db
         .prepare("UPDATE accounts SET details = ? WHERE id = ?")
         .run(
-          JSON.stringify({ summary: this.bio }),
+          JSON.stringify({ summary: this.character.bio }),
           getUuid(this.client.user?.id as string)
         );
     }
@@ -894,96 +940,96 @@ export class DiscordClient extends EventEmitter {
     );
   }
 
-  private async handleSetNameCommand(interaction: any) {
-    const newName = interaction.options.get("name")?.value;
-    const agentId = getUuid(settings.DISCORD_APPLICATION_ID as string) as UUID;
-    const userIdUUID = getUuid(interaction.user.id) as UUID;
-    const userName = interaction.user.username;
-    const room_id = getUuid(interaction.channelId) as UUID;
+  // private async handleSetNameCommand(interaction: any) {
+  //   const newName = interaction.options.get("name")?.value;
+  //   const agentId = getUuid(settings.DISCORD_APPLICATION_ID as string) as UUID;
+  //   const userIdUUID = getUuid(interaction.user.id) as UUID;
+  //   const userName = interaction.user.username;
+  //   const room_id = getUuid(interaction.channelId) as UUID;
 
-    await interaction.deferReply();
+  //   await interaction.deferReply();
 
-    await this.ensureUserExists(
-      agentId,
-      await this.fetchBotName(settings.DISCORD_API_TOKEN),
-      settings.DISCORD_API_TOKEN
-    );
-    await this.ensureUserExists(userIdUUID, userName);
-    await this.agent.ensureRoomExists(room_id);
-    await this.agent.ensureParticipantInRoom(userIdUUID, room_id);
-    await this.agent.ensureParticipantInRoom(agentId, room_id);
+  //   await this.ensureUserExists(
+  //     agentId,
+  //     await this.fetchBotName(settings.DISCORD_API_TOKEN),
+  //     settings.DISCORD_API_TOKEN
+  //   );
+  //   await this.ensureUserExists(userIdUUID, userName);
+  //   await this.agent.ensureRoomExists(room_id);
+  //   await this.agent.ensureParticipantInRoom(userIdUUID, room_id);
+  //   await this.agent.ensureParticipantInRoom(agentId, room_id);
 
-    if (newName) {
-      try {
-        adapter.db
-          .prepare("UPDATE accounts SET name = ? WHERE id = ?")
-          .run(newName, getUuid(interaction.client.user?.id));
+  //   if (newName) {
+  //     try {
+  //       adapter.db
+  //         .prepare("UPDATE accounts SET name = ? WHERE id = ?")
+  //         .run(newName, getUuid(interaction.client.user?.id));
 
-        const guild = interaction.guild;
-        if (guild) {
-          const botMember = await guild.members.fetch(
-            interaction.client.user?.id as string
-          );
-          await botMember.setNickname(newName as string);
-        }
+  //       const guild = interaction.guild;
+  //       if (guild) {
+  //         const botMember = await guild.members.fetch(
+  //           interaction.client.user?.id as string
+  //         );
+  //         await botMember.setNickname(newName as string);
+  //       }
 
-        await interaction.editReply(
-          `Agent's name has been updated to: ${newName}`
-        );
-      } catch (error) {
-        console.error("Error updating agent name:", error);
-        await interaction.editReply(
-          "An error occurred while updating the agent name."
-        );
-      }
-    } else {
-      await interaction.editReply(
-        "Please provide a new name for the agent."
-      );
-    }
-  }
+  //       await interaction.editReply(
+  //         `Agent's name has been updated to: ${newName}`
+  //       );
+  //     } catch (error) {
+  //       console.error("Error updating agent name:", error);
+  //       await interaction.editReply(
+  //         "An error occurred while updating the agent name."
+  //       );
+  //     }
+  //   } else {
+  //     await interaction.editReply(
+  //       "Please provide a new name for the agent."
+  //     );
+  //   }
+  // }
 
-  private async handleSetBioCommand(interaction: any) {
-    const newBio = interaction.options.get("bio")?.value;
-    if (newBio) {
-      try {
-        const agentId = getUuid(settings.DISCORD_APPLICATION_ID as string) as UUID;
-        const userIdUUID = getUuid(interaction.user.id) as UUID;
-        const userName = interaction.user.username;
-        const room_id = getUuid(interaction.channelId) as UUID;
+  // private async handleSetBioCommand(interaction: any) {
+  //   const newBio = interaction.options.get("bio")?.value;
+  //   if (newBio) {
+  //     try {
+  //       const agentId = getUuid(settings.DISCORD_APPLICATION_ID as string) as UUID;
+  //       const userIdUUID = getUuid(interaction.user.id) as UUID;
+  //       const userName = interaction.user.username;
+  //       const room_id = getUuid(interaction.channelId) as UUID;
 
-        await interaction.deferReply();
+  //       await interaction.deferReply();
 
-        await this.ensureUserExists(
-          agentId,
-          await this.fetchBotName(settings.DISCORD_API_TOKEN),
-          settings.DISCORD_API_TOKEN
-        );
-        await this.ensureUserExists(userIdUUID, userName);
-        await this.agent.ensureRoomExists(room_id);
-        await this.agent.ensureParticipantInRoom(userIdUUID, room_id);
-        await this.agent.ensureParticipantInRoom(agentId, room_id);
+  //       await this.ensureUserExists(
+  //         agentId,
+  //         await this.fetchBotName(settings.DISCORD_API_TOKEN),
+  //         settings.DISCORD_API_TOKEN
+  //       );
+  //       await this.ensureUserExists(userIdUUID, userName);
+  //       await this.agent.ensureRoomExists(room_id);
+  //       await this.agent.ensureParticipantInRoom(userIdUUID, room_id);
+  //       await this.agent.ensureParticipantInRoom(agentId, room_id);
 
-        adapter.db
-          .prepare("UPDATE accounts SET details = ? WHERE id = ?")
-          .run(
-            JSON.stringify({ summary: newBio }),
-            getUuid(interaction.client.user?.id)
-          );
+  //       adapter.db
+  //         .prepare("UPDATE accounts SET details = ? WHERE id = ?")
+  //         .run(
+  //           JSON.stringify({ summary: newBio }),
+  //           getUuid(interaction.client.user?.id)
+  //         );
 
-        await interaction.editReply(
-          `Agent's bio has been updated to: ${newBio}`
-        );
-      } catch (error) {
-        console.error("Error updating agent bio:", error);
-        await interaction.editReply(
-          "An error occurred while updating the agent bio."
-        );
-      }
-    } else {
-      await interaction.reply("Please provide a new bio for the agent.");
-    }
-  }
+  //       await interaction.editReply(
+  //         `Agent's bio has been updated to: ${newBio}`
+  //       );
+  //     } catch (error) {
+  //       console.error("Error updating agent bio:", error);
+  //       await interaction.editReply(
+  //         "An error occurred while updating the agent bio."
+  //       );
+  //     }
+  //   } else {
+  //     await interaction.reply("Please provide a new bio for the agent.");
+  //   }
+  // }
 
   private async handleJoinChannelCommand(interaction: any) {
     const channelId = interaction.options.get("channel")?.value as string;
--- src/clients/twitter/base.ts
+++ src/clients/twitter/base.ts
@@ -16,7 +16,6 @@ import settings from "../../core/settings.ts";
 
 import { fileURLToPath } from 'url';
 import ImageRecognitionService from "../../services/imageRecognition.ts";
-import { extractAnswer } from "../../core/util.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -62,7 +61,7 @@ export class ClientBase extends EventEmitter {
     this.directions = "- " + character.style.all.join("\n- ") + "- " + character.style.post.join()
     this.callback = callback;
     this.model = model;
-    this.imageRecognitionService = new ImageRecognitionService();
+    this.imageRecognitionService = new ImageRecognitionService(this.agent);
   
     // Check for Twitter cookies
     if (settings.TWITTER_COOKIES) {
@@ -124,9 +123,7 @@ export class ClientBase extends EventEmitter {
 
   async describeImage(imageUrl: string): Promise<string> {
     try {
-      const recognizedText = await this.imageRecognitionService.recognizeImage(imageUrl);
-      const description = extractAnswer(recognizedText[0]);
-
+      const description = await this.imageRecognitionService.recognizeImage(imageUrl);
       return description || 'Unable to describe the image.';
     } catch (error) {
       console.error('Error describing image:', error);
