b3c5c4b07672f6f67fa3723057cb01defa58ec02
Léo <leo@crowdform.co.uk>
2024-10-30 18:24:58 +0000

    add postgres support and schema

--- src/providers/token.ts
+++ src/providers/token.ts
@@ -1,8 +1,9 @@
-import { Connection } from "@solana/web3.js";
+import { Connection, PublicKey, ParsedAccountData } from "@solana/web3.js";
 // import fetch from "cross-fetch";
 import { IAgentRuntime, Memory, Provider, State } from "../core/types";
 import settings from "../core/settings";
 import BigNumber from "bignumber.js";
+import { TOKEN_PROGRAM_ID, AccountLayout } from "@solana/spl-token";
 import {
   ProcessedTokenData,
   TokenSecurityData,
@@ -756,7 +757,7 @@ const tokenProvider: Provider = {
     _state?: State
   ): Promise<string> => {
     try {
-      const provider = new TokenProvider(tokenAddress);
+      const provider = new TokenProvider(/*connection,*/ tokenAddress);
       return provider.getFormattedTokenReport();
     } catch (error) {
       console.error("Error fetching token data:", error);
