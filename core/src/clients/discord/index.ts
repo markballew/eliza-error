3a90ec2b9510977296c38f9827744afb282b37e5
Malibu <70625671+MarcoMandar@users.noreply.github.com>
2024-11-02 14:36:42 +0200

    Merge pull request #13 from ai16z/main

--- core/src/clients/discord/index.ts
+++ core/src/clients/discord/index.ts
@@ -10,22 +10,20 @@ import {
     User,
 } from "discord.js";
 import { EventEmitter } from "events";
-import { stringToUuid } from "../../core/uuid.ts";
-import { commands } from "./commands.ts";
-
 import { embeddingZeroVector } from "../../core/memory.ts";
-import { MessageManager } from "./messages.ts";
-import { VoiceManager } from "./voice.ts";
-
 import { Character, IAgentRuntime } from "../../core/types.ts";
+import { stringToUuid } from "../../core/uuid.ts";
 import chat_with_attachments from "./actions/chat_with_attachments.ts";
+import download_media from "./actions/download_media.ts";
 import joinvoice from "./actions/joinvoice.ts";
 import leavevoice from "./actions/leavevoice.ts";
 import summarize from "./actions/summarize_conversation.ts";
 import transcribe_media from "./actions/transcribe_media.ts";
-import download_media from "./actions/download_media.ts";
+import { commands } from "./commands.ts";
+import { MessageManager } from "./messages.ts";
 import channelStateProvider from "./providers/channelState.ts";
 import voiceStateProvider from "./providers/voiceState.ts";
+import { VoiceManager } from "./voice.ts";
 
 export class DiscordClient extends EventEmitter {
     apiToken: string;
@@ -37,9 +35,7 @@ export class DiscordClient extends EventEmitter {
 
     constructor(runtime: IAgentRuntime) {
         super();
-        this.apiToken = runtime.getSetting(
-            "DISCORD_API_TOKEN_" + runtime.character.name.toUpperCase()
-        ) as string;
+        this.apiToken = runtime.getSetting("DISCORD_API_TOKEN") as string;
         this.client = new Client({
             intents: [
                 GatewayIntentBits.Guilds,
@@ -122,10 +118,7 @@ export class DiscordClient extends EventEmitter {
             try {
                 await rest.put(
                     Routes.applicationCommands(
-                        this.runtime.getSetting(
-                            "DISCORD_APPLICATION_ID_" +
-                                this.runtime.character.name.toUpperCase()
-                        )
+                        this.runtime.getSetting("DISCORD_APPLICATION_ID")
                     ),
                     { body: commands }
                 );
