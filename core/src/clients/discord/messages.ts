6e48357d840fd91e1eb16f67813e94c6b34dc081
Jerad Bitner <sirkitree@gmail.com>
2024-11-03 14:35:37 -0800

    Create generate-changelog.yml

--- core/src/clients/discord/messages.ts
+++ core/src/clients/discord/messages.ts
@@ -171,7 +171,12 @@ export class MessageManager {
                     : undefined,
             };
 
-            const userMessage = { content, userId: userIdUUID, roomId };
+            const userMessage = {
+                content,
+                userId: userIdUUID,
+                agentId: this.runtime.agentId,
+                roomId,
+            };
 
             let state = (await this.runtime.composeState(userMessage, {
                 discordClient: this.client,
@@ -185,6 +190,7 @@ export class MessageManager {
                 id: stringToUuid(message.id),
                 ...userMessage,
                 userId: userIdUUID,
+                agentId: this.runtime.agentId,
                 roomId,
                 content,
                 createdAt: message.createdTimestamp,
@@ -277,6 +283,7 @@ export class MessageManager {
                     const memory: Memory = {
                         id: stringToUuid(message.id),
                         userId: this.runtime.agentId,
+                        agentId: this.runtime.agentId,
                         content,
                         roomId,
                         embedding: embeddingZeroVector,
@@ -306,6 +313,7 @@ export class MessageManager {
                         const memory: Memory = {
                             id: stringToUuid(m.id),
                             userId: this.runtime.agentId,
+                            agentId: this.runtime.agentId,
                             content: {
                                 ...content,
                                 action,
@@ -358,6 +366,8 @@ export class MessageManager {
 
     async cacheMessages(channel: TextChannel, count: number = 20) {
         const messages = await channel.messages.fetch({ limit: count });
+
+        // TODO: This is throwing an error but seems to work?
         for (const [_, message] of messages) {
             await this.handleMessage(message);
         }
