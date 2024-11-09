8cbd0c3877fc859ad34ec49a1d7a960b297a05af
Michael <m.sharpe@project89.org>
2024-11-09 13:40:03 -0800

    Support system prompt and google models

--- packages/core/src/models.ts
+++ packages/core/src/models.ts
@@ -137,9 +137,9 @@ const models: Models = {
             temperature: 0.3,
         },
         model: {
-            [ModelClass.SMALL]: "gemini-1.5-flash",
-            [ModelClass.MEDIUM]: "gemini-1.5-flash",
-            [ModelClass.LARGE]: "gemini-1.5-pro",
+            [ModelClass.SMALL]: "gemini-1.5-flash-latest",
+            [ModelClass.MEDIUM]: "gemini-1.5-flash-latest",
+            [ModelClass.LARGE]: "gemini-1.5-pro-latest",
             [ModelClass.EMBEDDING]: "text-embedding-004",
         },
     },
@@ -172,7 +172,7 @@ const models: Models = {
             presence_penalty: 0.0,
             temperature: 0.6,
         },
-        // Available models: https://docs.red-pill.ai/get-started/supported-models
+        // Available models: https://openrouter.ai/models
         // To test other models, change the models below
         model: {
             [ModelClass.SMALL]:
@@ -187,8 +187,7 @@ const models: Models = {
                 settings.LARGE_OPENROUTER_MODEL ||
                 settings.OPENROUTER_MODEL ||
                 "nousresearch/hermes-3-llama-3.1-405b",
-            [ModelClass.EMBEDDING]: 
-                "text-embedding-3-small",
+            [ModelClass.EMBEDDING]: "text-embedding-3-small",
         },
     },
     [ModelProviderName.OLLAMA]: {
