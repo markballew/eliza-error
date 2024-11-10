c295d172938e6c98a9e5d622b133ff199bed05fe
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2024-11-10 11:04:33 +1100

    Merge branch 'main' of https://github.com/ai16z/eliza

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -53,9 +53,9 @@ const sidebars = {
       collapsed: false,
       items: [
         "community/creator-fund",
-        "community/stream-notes",
+        "community/notes",
         "community/changelog",
-	"community/faq",
+        "community/faq",
         "community/contributing",
       ],
     },
