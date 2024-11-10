cb1fe46a8c412f8f4865b81c473719aa021c05e4
v1xingyue <qixingyue@gmail.com>
2024-11-10 21:25:08 +0800

    commit default from process.env.verbose

--- packages/core/src/logger.ts
+++ packages/core/src/logger.ts
@@ -1,4 +1,9 @@
 class ElizaLogger {
+    constructor() {
+        this.verbose = process.env.verbose === "true" || false;
+    }
+
+    verbose = false;
     closeByNewLine = true;
     useIcons = true;
     logsTitle = "LOGS";
@@ -214,6 +219,7 @@ class ElizaLogger {
         }
     }
     debug(...strings) {
+        if (!this.verbose) return;
         const fg = "magenta";
         const bg = "";
         const icon = "\u1367";
