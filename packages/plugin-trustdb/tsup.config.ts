f3227f29846b9c6ca905e5b3dac4f0d524f09133
Ophiuchus <at.o.on.x@proton.me>
2024-11-18 22:57:15 -0600

    removed ollama embeddings. fastembeddings or openai only

--- packages/plugin-trustdb/tsup.config.ts
+++ packages/plugin-trustdb/tsup.config.ts
@@ -6,8 +6,5 @@ export default defineConfig({
     sourcemap: true,
     clean: true,
     format: ["esm"], // Ensure you're targeting CommonJS
-    external: [
-        "@0glabs/0g-ts-sdk",
-        // Add other modules you want to externalize
-    ],
+    external: [],
 });
