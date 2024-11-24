949d9b135992c119cfd50af214f07f8be888f429
sayangel <sayangel@gmail.com>
2024-11-24 15:22:27 -0800

    feat: working farcaster client with neynar

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -25,10 +25,7 @@ import {
 import { bootstrapPlugin } from "@ai16z/plugin-bootstrap";
 import { solanaPlugin } from "@ai16z/plugin-solana";
 import { nodePlugin } from "@ai16z/plugin-node";
-import {
-    coinbaseCommercePlugin,
-    coinbaseMassPaymentsPlugin,
-} from "@ai16z/plugin-coinbase";
+import { coinbaseCommercePlugin } from "@ai16z/plugin-coinbase";
 import Database from "better-sqlite3";
 import fs from "fs";
 import readline from "readline";
@@ -256,12 +253,6 @@ export function createAgent(
             character.settings.secrets?.COINBASE_COMMERCE_KEY ||
             process.env.COINBASE_COMMERCE_KEY
                 ? coinbaseCommercePlugin
-                : null,
-            (character.settings.secrets?.COINBASE_API_KEY ||
-                process.env.COINBASE_API_KEY) &&
-            (character.settings.secrets?.COINBASE_PRIVATE_KEY ||
-                process.env.COINBASE_PRIVATE_KEY)
-                ? coinbaseMassPaymentsPlugin
                 : null,
         ].filter(Boolean),
         providers: [],
