48a2a33a6c4e14e32bb457483ef6ca129d60c847
cygaar <tmpftl1@gmail.com>
2024-11-27 23:01:24 -0500

    Better browser settings

--- packages/plugin-solana/src/providers/trustScoreProvider.ts
+++ packages/plugin-solana/src/providers/trustScoreProvider.ts
@@ -140,19 +140,14 @@ export class TrustScoreManager {
 
         return {
             tokenPerformance: {
-                tokenAddress:
-                    processedData.dexScreenerData.pairs[0]?.baseToken.address ||
+                tokenAddress: processedData.dexScreenerData.pairs[0]?.baseToken.address ||
                     "",
-                priceChange24h:
-                    processedData.tradeData.price_change_24h_percent,
+                priceChange24h: processedData.tradeData.price_change_24h_percent,
                 volumeChange24h: processedData.tradeData.volume_24h,
-                trade_24h_change:
-                    processedData.tradeData.trade_24h_change_percent,
-                liquidity:
-                    processedData.dexScreenerData.pairs[0]?.liquidity.usd || 0,
+                trade_24h_change: processedData.tradeData.trade_24h_change_percent,
+                liquidity: processedData.dexScreenerData.pairs[0]?.liquidity.usd || 0,
                 liquidityChange24h: 0,
-                holderChange24h:
-                    processedData.tradeData.unique_wallet_24h_change_percent,
+                holderChange24h: processedData.tradeData.unique_wallet_24h_change_percent,
                 rugPull: false,
                 isScam: processedData.tokenCodex.isScam,
                 marketCapChange24h: 0,
@@ -161,9 +156,9 @@ export class TrustScoreManager {
                 suspiciousVolume: suspiciousVolume,
                 validationTrust: validationTrustScore,
                 balance: balance,
-                initialMarketCap:
-                    processedData.dexScreenerData.pairs[0]?.marketCap || 0,
+                initialMarketCap: processedData.dexScreenerData.pairs[0]?.marketCap || 0,
                 lastUpdated: new Date(),
+                symbol: ""
             },
             recommenderMetrics: {
                 recommenderId: recommenderId,
@@ -416,6 +411,7 @@ export class TrustScoreManager {
 
         this.trustScoreDb.upsertTokenPerformance({
             tokenAddress: tokenAddress,
+            symbol: processedData.tokenCodex.symbol,
             priceChange24h: processedData.tradeData.price_change_24h_percent,
             volumeChange24h: processedData.tradeData.volume_24h,
             trade_24h_change: processedData.tradeData.trade_24h_change_percent,
@@ -453,7 +449,10 @@ export class TrustScoreManager {
             };
             this.trustScoreDb.addTransaction(transaction);
         }
-        this.simulationSellingService.processTokenPerformance(tokenAddress);
+        this.simulationSellingService.processTokenPerformance(
+            tokenAddress,
+            recommenderId
+        );
         // api call to update trade performance
         this.createTradeInBe(tokenAddress, recommenderId, username, data);
         return creationData;
@@ -531,7 +530,7 @@ export class TrustScoreManager {
         const processedData: ProcessedTokenData =
             await this.tokenProvider.getProcessedTokenData();
         const wallet = new WalletProvider(
-            new Connection("https://api.mainnet-beta.solana.com"),
+            this.connection,
             new PublicKey(Wallet!)
         );
         const prices = await wallet.fetchPrices(runtime);
