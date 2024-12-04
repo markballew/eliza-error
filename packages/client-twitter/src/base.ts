dbdd4d40457c246af4582834a2bf0f6c3f78199c
HashWarlock <hashwarlock@protonmail.com>
2024-12-04 09:56:00 -0600

    update .env.example with TEE_MODE

--- packages/client-twitter/src/base.ts
+++ packages/client-twitter/src/base.ts
@@ -617,7 +617,7 @@ export class ClientBase extends EventEmitter {
         await this.runtime.cacheManager.set(
             `twitter/${this.profile.username}/timeline`,
             timeline,
-            { expires: Date.now() + 10 * 1000 }
+            { expires: 10 * 1000 }
         );
     }
 
@@ -625,7 +625,7 @@ export class ClientBase extends EventEmitter {
         await this.runtime.cacheManager.set(
             `twitter/${this.profile.username}/mentions`,
             mentions,
-            { expires: Date.now() + 10 * 1000 }
+            { expires: 10 * 1000 }
         );
     }
 
