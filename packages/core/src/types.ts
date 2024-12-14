f10cabefc2e02fba0cb5505d186c6bded6be8f60
Ninja Dev (QI) <142059473+azep-ninja@users.noreply.github.com>
2024-12-14 11:13:41 -0700

    Merge branch 'develop' into discord-telegram-team-agents-and-optimizations

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -724,6 +724,7 @@ export type Character = {
         discord?: {
             shouldIgnoreBotMessages?: boolean;
             shouldIgnoreDirectMessages?: boolean;
+            shouldRespondOnlyToMentions?: boolean;
             messageSimilarityThreshold?: number;
             isPartOfTeam?: boolean;
             teamAgentIds?: string[];
@@ -733,6 +734,9 @@ export type Character = {
         telegram?: {
             shouldIgnoreBotMessages?: boolean;
             shouldIgnoreDirectMessages?: boolean;
+            shouldRespondOnlyToMentions?: boolean;
+            shouldOnlyJoinInAllowedGroups?: boolean;
+            allowedGroupIds?: string[];
             messageSimilarityThreshold?: number;
             isPartOfTeam?: boolean;
             teamAgentIds?: string[];
@@ -742,6 +746,7 @@ export type Character = {
         slack?: {
             shouldIgnoreBotMessages?: boolean;
             shouldIgnoreDirectMessages?: boolean;
+
         };
     };
 
