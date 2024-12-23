43aec1ce9508b1c6fba9502dcfa99645aa3b3ec0
Agustin Armellini Fischer <armellini13@gmail.com>
2024-12-23 15:48:36 +0100

    Improve GOAT integration by allowing tool calling when using generateText

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -55,7 +55,6 @@ import { suiPlugin } from "@elizaos/plugin-sui";
 import { TEEMode, teePlugin } from "@elizaos/plugin-tee";
 import { tonPlugin } from "@elizaos/plugin-ton";
 import { zksyncEraPlugin } from "@elizaos/plugin-zksync-era";
-import { verifiableLogPlugin } from "@elizaos/plugin-tee-verifiable-log";
 import Database from "better-sqlite3";
 import fs from "fs";
 import path from "path";
@@ -490,7 +489,7 @@ export async function createAgent(
     }
 
     let goatPlugin: any | undefined;
-    if (getSecret(character, "ALCHEMY_API_KEY")) {
+    if (getSecret(character, "EVM_PRIVATE_KEY")) {
         goatPlugin = await createGoatPlugin((secret) =>
             getSecret(character, secret)
         );
@@ -561,7 +560,7 @@ export async function createAgent(
             getSecret(character, "COINBASE_NOTIFICATION_URI")
                 ? webhookPlugin
                 : null,
-            getSecret(character, "ALCHEMY_API_KEY") ? goatPlugin : null,
+            goatPlugin,
             getSecret(character, "FLOW_ADDRESS") &&
             getSecret(character, "FLOW_PRIVATE_KEY")
                 ? flowPlugin
@@ -572,9 +571,6 @@ export async function createAgent(
             getSecret(character, "TON_PRIVATE_KEY") ? tonPlugin : null,
             getSecret(character, "SUI_PRIVATE_KEY") ? suiPlugin : null,
             getSecret(character, "STORY_PRIVATE_KEY") ? storyPlugin : null,
-            (teeMode !== TEEMode.OFF && walletSecretSalt &&getSecret(character,"VLOG")
-                ? verifiableLogPlugin
-                : null)
         ].filter(Boolean),
         providers: [],
         actions: [],
