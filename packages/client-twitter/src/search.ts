965377e5c8e76c53daec86cd5bf8dbec41fff82b
Shakker Nerd <165377636+shakkernerd@users.noreply.github.com>
2024-12-24 23:54:48 +0000

    Merge branch 'develop' into 1223-fix-todos

--- packages/client-twitter/src/search.ts
+++ packages/client-twitter/src/search.ts
@@ -1,5 +1,5 @@
 import { SearchMode } from "agent-twitter-client";
-import {composeContext, elizaLogger} from "@elizaos/core";
+import { composeContext } from "@elizaos/core";
 import { generateMessageResponse, generateText } from "@elizaos/core";
 import { messageCompletionFooter } from "@elizaos/core";
 import {
@@ -59,12 +59,10 @@ export class TwitterSearchClient {
     }
 
     private engageWithSearchTermsLoop() {
-        this.engageWithSearchTerms().then();
-        const randomMinutes = (Math.floor(Math.random() * (120 - 60 + 1)) + 60);
-        elizaLogger.log(`Next twitter search scheduled in ${randomMinutes} minutes`);
+        this.engageWithSearchTerms();
         setTimeout(
             () => this.engageWithSearchTermsLoop(),
-            randomMinutes * 60 * 1000
+            (Math.floor(Math.random() * (120 - 60 + 1)) + 60) * 60 * 1000
         );
     }
 
