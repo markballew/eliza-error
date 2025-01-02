472b8c0e50f3da11c9b9b5f2e1f7668caffa050f
norbert-kulus-blockydevs <norbert.kulus@blockydevs.com>
2025-01-02 14:06:39 +0100

    [ELIZAAI-16](feat): cosmos wallet provider with balances

--- packages/plugin-solana/src/actions/fomo.ts
+++ packages/plugin-solana/src/actions/fomo.ts
@@ -1,4 +1,4 @@
-import { generateImage } from "@elizaos/core";
+import { generateImage } from "@ai16z/eliza";
 import {
     Connection,
     Keypair,
@@ -20,7 +20,7 @@ import {
     generateObject,
     composeContext,
     type Action,
-} from "@elizaos/core";
+} from "@ai16z/eliza";
 
 import { walletProvider } from "../providers/wallet.ts";
 
--- packages/plugin-web-search/src/index.ts
+++ packages/plugin-web-search/src/index.ts
@@ -1,4 +1,4 @@
-import { elizaLogger } from "@elizaos/core";
+import { elizaLogger } from "@ai16z/eliza";
 import {
     Action,
     HandlerCallback,
@@ -6,9 +6,9 @@ import {
     Memory,
     Plugin,
     State,
-} from "@elizaos/core";
-import { generateWebSearch } from "@elizaos/core";
-import { SearchResult } from "@elizaos/core";
+} from "@ai16z/eliza";
+import { generateWebSearch } from "@ai16z/eliza";
+import { SearchResult } from "@ai16z/eliza";
 import { encodingForModel, TiktokenModel } from "js-tiktoken";
 
 const DEFAULT_MAX_WEB_SEARCH_TOKENS = 4000;
@@ -17,20 +17,21 @@ const DEFAULT_MODEL_ENCODING = "gpt-3.5-turbo";
 function getTotalTokensFromString(
     str: string,
     encodingName: TiktokenModel = DEFAULT_MODEL_ENCODING
-) {
+  ) {
     const encoding = encodingForModel(encodingName);
     return encoding.encode(str).length;
-}
+  }
 
 function MaxTokens(
     data: string,
     maxTokens: number = DEFAULT_MAX_WEB_SEARCH_TOKENS
-): string {
+  ): string {
+    
     if (getTotalTokensFromString(data) >= maxTokens) {
         return data.slice(0, maxTokens);
     }
     return data;
-}
+  }
 
 const webSearch: Action = {
     name: "WEB_SEARCH",
@@ -207,5 +208,3 @@ export const webSearchPlugin: Plugin = {
     evaluators: [],
     providers: [],
 };
-
-export default webSearchPlugin;
