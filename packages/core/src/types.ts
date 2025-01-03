8186aec12827bb05abe99803df75b8005473e1d8
Shakker Nerd <165377636+shakkernerd@users.noreply.github.com>
2025-01-03 13:07:51 +0000

    Merge branch 'develop' into Docs-Patch-1

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1261,25 +1261,6 @@ export interface IAwsS3Service extends Service {
     generateSignedUrl(fileName: string, expiresIn: number): Promise<string>;
 }
 
-
-export interface UploadIrysResult {
-    success: boolean;
-    url?: string;
-    error?: string;
-}
-
-export interface DataIrysFetchedFromGQL {
-    success: boolean;
-    data: any;
-    error?: string;
-}
-
-export interface IIrysService extends Service {
-    uploadDataOnIrys(data: any): Promise<UploadIrysResult>;
-    getDataFromAnAgent(agentsWalletPublicKeys: string[]): Promise<DataIrysFetchedFromGQL>;
-    uploadFileOrImageOnIrys(data: string): Promise<UploadIrysResult>;
-}
-
 export type SearchImage = {
     url: string;
     description?: string;
@@ -1314,7 +1295,6 @@ export enum ServiceType {
     AWS_S3 = "aws_s3",
     BUTTPLUG = "buttplug",
     SLACK = "slack",
-    IRYS = "irys",
 }
 
 export enum LoggingLevel {
