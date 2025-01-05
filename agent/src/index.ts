dd90eee43a51bc0416aea51134e831c7e7c20d81
Joey <joey.kheireddine@outlook.com>
2025-01-05 23:41:56 +0100

    upd: fixed broken API Documentation URL

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -67,7 +67,6 @@ import { webSearchPlugin } from "@elizaos/plugin-web-search";
 import { stargazePlugin } from "@elizaos/plugin-stargaze";
 import { zksyncEraPlugin } from "@elizaos/plugin-zksync-era";
 import { availPlugin } from "@elizaos/plugin-avail";
-import { openWeatherPlugin } from "@elizaos/plugin-open-weather";
 import Database from "better-sqlite3";
 import fs from "fs";
 import net from "net";
@@ -625,9 +624,6 @@ export async function createAgent(
                 : null,
             getSecret(character, "AVAIL_SEED") ? availPlugin : null,
             getSecret(character, "AVAIL_APP_ID") ? availPlugin : null,
-            getSecret(character, "OPEN_WEATHER_API_KEY")
-                ? openWeatherPlugin
-                : null,
         ].filter(Boolean),
         providers: [],
         actions: [],
