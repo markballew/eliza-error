3414d8181be1d182413bcb11fcf59cd83204c662
Joey <joey.kheireddine@outlook.com>
2025-01-08 15:40:38 +0100

    upd: audio-recorder operational

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,49 @@
-import "./App.css";
-import Agents from "./Agents";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "./components/app-sidebar";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { Toaster } from "./components/ui/toaster";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Chat from "./routes/chat";
+import Overview from "./routes/overview";
+import Home from "./routes/home";
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center p-4">
-            <Agents />
+        <div
+            className="dark"
+            style={{
+                colorScheme: "dark",
+            }}
+        >
+            <BrowserRouter>
+                <TooltipProvider delayDuration={0}>
+                    <QueryClientProvider client={queryClient}>
+                        <SidebarProvider>
+                            <AppSidebar />
+                            <SidebarInset>
+                                <div className="flex flex-1 flex-col gap-4 size-full">
+                                    <Routes>
+                                        <Route path="/" element={<Home />} />
+                                        <Route
+                                            path="chat/:agentId"
+                                            element={<Chat />}
+                                        />
+                                        <Route
+                                            path="settings/:agentId"
+                                            element={<Overview />}
+                                        />
+                                    </Routes>
+                                </div>
+                            </SidebarInset>
+                        </SidebarProvider>
+                        <Toaster />
+                    </QueryClientProvider>
+                </TooltipProvider>
+            </BrowserRouter>
         </div>
     );
 }
