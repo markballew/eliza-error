90ebaccf0008a0285eee43f05d992af429ce3436
Luka Petrovic <lucapetro93@gmail.com>
2025-01-10 10:55:25 +0100

    feat: get gainers and losers

--- packages/plugin-coingecko/src/index.ts
+++ packages/plugin-coingecko/src/index.ts
@@ -1,12 +1,16 @@
 import { Plugin } from "@elizaos/core";
+import getMarkets from "./actions/getMarkets";
 import getPrice from "./actions/getPrice";
+import getTopGainersLosers from "./actions/getTopGainersLosers";
+import getTrending from "./actions/getTrending";
+import { categoriesProvider } from "./providers/categoriesProvider";
 
 export const coingeckoPlugin: Plugin = {
     name: "coingecko",
     description: "CoinGecko Plugin for Eliza",
-    actions: [getPrice],
+    actions: [getPrice, getTrending, getMarkets, getTopGainersLosers],
     evaluators: [],
-    providers: [],
+    providers: [categoriesProvider],
 };
 
 export default coingeckoPlugin;
