28078ced9ac7ccff77acba98f7f7b01846fbf06a
Sayo <82053242+wtfsayo@users.noreply.github.com>
2025-01-10 16:43:47 +0530

    revert #1808

--- packages/plugin-coingecko/src/index.ts
+++ packages/plugin-coingecko/src/index.ts
@@ -1,16 +1,12 @@
 import { Plugin } from "@elizaos/core";
-import getMarkets from "./actions/getMarkets";
 import getPrice from "./actions/getPrice";
-import getTopGainersLosers from "./actions/getTopGainersLosers";
-import getTrending from "./actions/getTrending";
-import { categoriesProvider } from "./providers/categoriesProvider";
 
 export const coingeckoPlugin: Plugin = {
     name: "coingecko",
     description: "CoinGecko Plugin for Eliza",
-    actions: [getPrice, getTrending, getMarkets, getTopGainersLosers],
+    actions: [getPrice],
     evaluators: [],
-    providers: [categoriesProvider],
+    providers: [],
 };
 
 export default coingeckoPlugin;
