6bf6f60eb4b63b48d0e91925098d2cf412993e5c
root <root@localhost.localdomain>
2025-01-10 20:24:39 +0800

    20250110 tts

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1260,25 +1260,6 @@ export interface IAwsS3Service extends Service {
     generateSignedUrl(fileName: string, expiresIn: number): Promise<string>;
 }
 
-
-export interface UploadIrysResult {
-    success: boolean;
-    url?: string;
-    error?: string;
-}
-
-export interface DataIrysFetchedFromGQL {
-    success: boolean;
-    data: any;
-    error?: string;
-}
-
-export interface IIrysService extends Service {
-    uploadDataOnIrys(data: any): Promise<UploadIrysResult>;
-    getDataFromAnAgent(agentsWalletPublicKeys: string[]): Promise<DataIrysFetchedFromGQL>;
-    uploadFileOrImageOnIrys(data: string): Promise<UploadIrysResult>;
-}
-
 export type SearchImage = {
     url: string;
     description?: string;
@@ -1313,7 +1294,6 @@ export enum ServiceType {
     AWS_S3 = "aws_s3",
     BUTTPLUG = "buttplug",
     SLACK = "slack",
-    IRYS = "irys",
 }
 
 export enum LoggingLevel {
