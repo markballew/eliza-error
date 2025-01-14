27edae8f951985d808d3ee61f1357f9686cad9cf
v1xingyue <qixingyue@gmail.com>
2025-01-14 17:35:06 +0800

    support load character from  character_url

--- packages/plugin-evm/src/providers/wallet.ts
+++ packages/plugin-evm/src/providers/wallet.ts
@@ -1,11 +1,8 @@
 import {
     createPublicClient,
-    createTestClient,
     createWalletClient,
     formatUnits,
     http,
-    publicActions,
-    walletActions,
 } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import {
@@ -24,7 +21,6 @@ import type {
     HttpTransport,
     Account,
     PrivateKeyAccount,
-    TestClient,
 } from "viem";
 import * as viemChains from "viem/chains";
 import { DeriveKeyProvider, TEEMode } from "@elizaos/plugin-tee";
@@ -86,16 +82,6 @@ export class WalletProvider {
         });
 
         return walletClient;
-    }
-
-    getTestClient(): TestClient {
-        return createTestClient({
-            chain: viemChains.hardhat,
-            mode: "hardhat",
-            transport: http(),
-        })
-            .extend(publicActions)
-            .extend(walletActions);
     }
 
     getChainConfigs(chainName: SupportedChain): Chain {
