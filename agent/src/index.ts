8231f1f9e7e3b8ee3c3c43f22c220fa076fcde86
lxcong <83766787@qq.com>
2025-01-21 16:05:43 +0800

    refactor: improve token balance calculation precision

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -113,6 +113,7 @@ import { devinPlugin } from '@elizaos/plugin-devin';
 
 
 import { zksyncEraPlugin } from "@elizaos/plugin-zksync-era";
+import { chainbasePlugin } from "@elizaos/plugin-chainbase";
 import Database from "better-sqlite3";
 import fs from "fs";
 import net from "net";
@@ -1072,6 +1073,7 @@ export async function createAgent(
             getSecret(character, "AKASH_WALLET_ADDRESS")
                 ? akashPlugin
                 : null,
+            getSecret(character, "CHAINBASE_API_KEY") ? chainbasePlugin : null,
             getSecret(character, "QUAI_PRIVATE_KEY") ? quaiPlugin : null,
             getSecret(character, "RESERVOIR_API_KEY")
                 ? createNFTCollectionsPlugin()
