31d73d5c990959cef95d5ebbd6e1d78dc7336812
CheddarQueso <cheddarqueso3d@gmail.com>
2025-01-24 12:51:12 -0500

    Merge remote-tracking branch 'upstream/develop' into news-plugin

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -86,6 +86,7 @@ import { imageGenerationPlugin } from "@elizaos/plugin-image-generation";
 import { lensPlugin } from "@elizaos/plugin-lensNetwork";
 import { multiversxPlugin } from "@elizaos/plugin-multiversx";
 import { nearPlugin } from "@elizaos/plugin-near";
+import { newsPlugin } from "@elizaos/plugin-news";
 import createNFTCollectionsPlugin from "@elizaos/plugin-nft-collections";
 import { nftGenerationPlugin } from "@elizaos/plugin-nft-generation";
 import { createNodePlugin } from "@elizaos/plugin-node";
