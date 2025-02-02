80e5c71f4a82855670cf82024a76ef66139584d4
Shakker Nerd <165377636+shakkernerd@users.noreply.github.com>
2025-02-02 23:11:21 +0000

    Merge pull request #3180 from AIFlowML/fix-multi-biome-01

--- packages/plugin-coingecko/src/index.ts
+++ packages/plugin-coingecko/src/index.ts
@@ -6,10 +6,8 @@ import getTopGainersLosers from "./actions/getTopGainersLosers";
 import getTrending from "./actions/getTrending";
 import getTrendingPools from "./actions/getTrendingPools";
 import getNewlyListed from "./actions/getNewlyListed";
-import getNetworkTrendingPools from "./actions/getNetworkTrendingPools";
 import { categoriesProvider } from "./providers/categoriesProvider";
 import { coinsProvider } from "./providers/coinsProvider";
-import { networksProvider } from "./providers/networkProvider";
 
 export const coingeckoPlugin: Plugin = {
     name: "coingecko",
@@ -22,10 +20,9 @@ export const coingeckoPlugin: Plugin = {
         getMarkets,
         getTopGainersLosers,
         getNewlyListed,
-        getNetworkTrendingPools,
     ],
     evaluators: [],
-    providers: [categoriesProvider, coinsProvider, networksProvider],
+    providers: [categoriesProvider, coinsProvider],
 };
 
 export default coingeckoPlugin;
