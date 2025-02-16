3474cf5178f60c9eec13f13ce09b44ac68d29fd0
s <60634884+tcm390@users.noreply.github.com>
2025-02-16 16:10:39 +0800

    Merge branch 'v2-develop' into tcm/fix-discord-test

--- packages/plugin-discord/src/test-suite.ts
+++ packages/plugin-discord/src/test-suite.ts
@@ -24,19 +24,19 @@ export class DiscordTestSuite implements TestSuite {
   constructor() {
     this.tests = [
       {
-        name: "test creating discord client",
+        name: "Initialize Discord Client",
         fn: this.testCreatingDiscordClient.bind(this),
       },
       {
-        name: "test voice manager playback",
+        name: "Voice Playback & TTS",
         fn: this.testTextToSpeechPlayback.bind(this),
       },
       {
-        name: "test message manager sending message with files",
+        name: "Send Message with Attachments",
         fn: this.testSendingTextMessage.bind(this),
       },
       {
-        name: "test message manager handleMessage",
+        name: "Handle Incoming Messages",
         fn: this.testHandlingMessage.bind(this),
       },
     ];
@@ -127,11 +127,11 @@ export class DiscordTestSuite implements TestSuite {
 
         audioPlayer.once("error", (error) => {
           reject(error);
-          throw new Error("TTS playback error:", error);
+          throw new Error(`TTS playback error: ${error}`);
         });
       });
     } catch (error) {
-      throw new Error("Error in TTS playback test:", error);
+      throw new Error(`Error in TTS playback test: ${error}`);
     }
   }
 
@@ -144,7 +144,7 @@ export class DiscordTestSuite implements TestSuite {
         "https://github.com/elizaOS/awesome-eliza/blob/main/assets/eliza-logo.jpg",
       ]);
     } catch (error) {
-      throw new Error("Error in sending text message:", error);
+      throw new Error(`Error in sending text message: ${error}`);
     }
   }
 
@@ -171,7 +171,7 @@ export class DiscordTestSuite implements TestSuite {
       };
       await this.discordClient.messageManager.handleMessage(fakeMessage as any);
     } catch (error) {
-      throw new Error("Error in sending text message:", error);
+      throw new Error(`Error in sending text message: ${error}`);
     }
   }
 
@@ -216,7 +216,7 @@ export class DiscordTestSuite implements TestSuite {
 
       return channel;
     } catch (error) {
-      throw new Error("Error fetching text channel:", error);
+      throw new Error(`Error fetching text channel: ${error}`);
     }
   }
 
@@ -237,7 +237,7 @@ export class DiscordTestSuite implements TestSuite {
         files
       );
     } catch (error) {
-      throw new Error("Error sending message:", error);
+      throw new Error(`Error sending message: ${error}`);
     }
   }
 }
